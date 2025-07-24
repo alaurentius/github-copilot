80c19c819e138eddf86b99b1759298e0b47911a5
nglcode <nglcode@mailbox.org>
2025-07-24 14:07:33 +0200

    basic react example added

--- react-basic-tut/src/App.js
+++ react-basic-tut/src/App.js
@@ -1,7 +1,35 @@
 
+import { useState } from 'react';
+import Accordion from './Accordion';
+import Profile from './Profile';
+
 function App() {
+  const [openAccordionId, setOpenAccordionId] = useState(null);
+
+  const handleAccordionClick = (accordionId) => {
+    setOpenAccordionId((prevValue) => {
+      return prevValue === accordionId ? null : accordionId;
+    });
+  };
+
   return (
-    <div></div>
+    <div>
+      <Accordion
+        id="my-profile"
+        labelText="My profile"
+        open={openAccordionId === 'my-profile'}
+        onClick={handleAccordionClick}
+      >
+        <Profile />
+      </Accordion>
+      <Accordion
+        id="my-hobbies"
+        labelText="My hobbies"
+        open={openAccordionId === 'my-hobbies'}
+        onClick={handleAccordionClick}>
+        New Accordion
+      </Accordion>
+    </div>
   );
 }
 
---
+++ react-basic-tut/src/Profile.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export default function Profile() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:3001/profile');
+            const data = await response.json();
+            setFirstName(data.firstName);
+            setLastName(data.lastName);
+        };
+        fetchData();
+    }, []);
+    return <div>
+        <div>
+            <strong>First Name:</strong> {firstName}
+        </div>
+        <div>
+            <strong>Last Name:</strong> {lastName}
+        </div>
+    </div>;
+}
